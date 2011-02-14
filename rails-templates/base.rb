3fbbae50b63ceaab68d2d87adf1ef7264cfe6e8b
BigCat <miaout17@gmail.com>
2011-02-14 11:04:25 +0800

    ujs

--- rails-templates/base.rb
+++ rails-templates/base.rb
@@ -1,3 +1,5 @@
+template_path = File.expand_path(File.join(__FILE__, '..'))
+
 run "cp config/database.yml config/database.yml.example"
 run "rm .gitignore"
 
@@ -18,11 +20,11 @@ gem "rspec-rails", "~> 2.0.0", :group => [:development, :test]
 gem "autotest", :group => [:development, :test]
 gem "rcov", :group => [:development, :test]
 
-if yes?("Use awesome_print, hirb and wirble? [yn]", :cyan)
-  gem "awesome_print", :group => [:development, :test]
-  gem "hirb", :group => [:development, :test]
-  gem "wirble", :group => [:development, :test]
-end
+# if yes?("Use awesome_print, hirb and wirble? [yn]", :cyan)
+#   gem "awesome_print", :group => [:development, :test]
+#   gem "hirb", :group => [:development, :test]
+#   gem "wirble", :group => [:development, :test]
+# end
 
 # run "bundle" # Or generator may stuck :(
 
@@ -36,7 +38,21 @@ EOS
 
 gsub_file('spec/spec_helper.rb', '/spec/fixtures', '/spec/factories')
 
+# UJS
+#Todo: automatically extract version?
+%w{jquery-1.5.min rails}.each do |name|
+  source = File.join(template_path, 'javascripts', "#{name}.js")
+  target = "public/javascripts/#{name}.js"
+  copy_file(source, target)
+end
+create_file("public/javascripts/#{app_name}.js", "")
+gsub_file 'config/application.rb',
+  /# config.action_view.javascript_expansions.*/,
+  "config.action_view.javascript_expansions[:defaults] = %w(jquery-1.5.min rails #{app_name})"
+
 git :init
 git :add => "."
 git :commit => "-m 'initial commit'"
+
+run "rm public/javascripts/*"
 
